982868726638a45686df86090ab9390f4b45049f
Francesco Ferrari <francescoferrari@Francescos-MacBook-Pro.local>
2017-03-22 19:23:38 -0700

    3:

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from deeplogo import DeepLogo
-app = Flask(__name__)
+import os
+ASSETS_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/assets')
+
+app = Flask(__name__,  static_folder=ASSETS_DIR)
+
 
 dl = DeepLogo()
 
@@ -11,6 +15,7 @@ def upload_file():
     brand = "NOT CLASSIFIED YET"
     if request.method == 'POST':
         text = request.form['text']
+        brand = text
         brand = dl.predict(text)
 
     return render_template('index.html', brand=brand)
